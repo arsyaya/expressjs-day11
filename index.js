7b4517debe77ac4b81e218d3d72b0f1df361154e
arsyaya <arsyajr9@gmail.com>
2023-08-28 19:53:32 +0700

    complete simple crud

--- index.js
+++ index.js
@@ -14,11 +14,36 @@ app.use(express.static("src/asset"));
 // parsing data from client
 app.use(express.urlencoded({ extended: false }));
 
-// routing
-app.get("/", home); // index
-app.get("/add-project", addProject); // add project
+// dummy data (fake data) for testing
+const dataBlog = [
+  {
+    // id: 1,
+    name: "Hari-hari Semangat Ngoding",
+    desc: "Ayo semuanya belajar ngoding ngopi standing yang mendang mending.",
+    author: "Arsya Rizvadwinafisa",
+    postAt: new Date(),
+  },
+  {
+    // id: 2,
+    name: "Mulai dengan nonton one piece",
+    desc: "New World kami datang, bersiaplah para sichibukai aku akan menjadi raja bajak laut.",
+    author: "Monkey D Luffy",
+    postAt: new Date(),
+  },
+];
+
+// routing render
+app.get("/", home); // index home
+app.get("/add-project", addProject); // add-project
+app.get("/blog", blog); // blog
+app.get("/blog-detail/:id", blogDetail); // blog-detail
 app.get("/contact", contactMe); // contact me
-app.post("/add-project", addBlog);
+app.get("/blog-edit/:id", editBlog); // halaman edit blog
+
+// routing proses
+app.post("/add-project", addBlog); // for post/create data in add-project
+app.post("/blog-edit/:id", updateBlog); // for send update or edit blog
+app.get("/delete-blog/:id", deleteBlog); // for delete blog data
 
 // local server
 app.listen(port, () => {
@@ -36,17 +61,64 @@ function addProject(req, res) {
   res.render("add-project");
 }
 
-// add new data blog
-function addBlog(req, res) {
-  const { name, desc } = req.body;
+// blog
+function blog(req, res) {
+  res.render("blog", { dataBlog });
+}
 
-  console.log(name);
-  console.log(desc);
-
-  res.redirect("/");
+// blog detail
+function blogDetail(req, res) {
+  const { id } = req.params;
+  res.render("blog-detail", { data: dataBlog[id] });
 }
 
 // contact me
 function contactMe(req, res) {
   res.render("contact-me");
+}
+
+// edit blog
+function editBlog(req, res) {
+  const { id } = req.params;
+  res.render("blog-edit", { data: dataBlog[id], id });
+}
+
+// add new data blog
+function addBlog(req, res) {
+  const { name, desc } = req.body;
+
+  const blog = {
+    name,
+    desc,
+    image: "blog-img-detail.png",
+    author: "Icang Markocang",
+    postAt: new Date(),
+  };
+
+  dataBlog.push(blog);
+
+  res.redirect("/blog");
+}
+
+// update edit
+function updateBlog(req, res) {
+  const { id } = req.params;
+
+  const { name, desc } = req.body;
+
+  dataBlog[id].name = name;
+  dataBlog[id].desc = desc;
+  console.log(dataBlog);
+  res.redirect("/blog");
+}
+
+// delete
+function deleteBlog(req, res) {
+  const { id } = req.params;
+
+  dataBlog.splice(id, 1);
+
+  console.log({ id });
+
+  res.redirect("/blog");
 }
